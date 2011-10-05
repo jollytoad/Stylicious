629a63bb25c3567e78223037b82151335b39cafa
Mark <jollytoad@gmail.com>
2011-10-05 08:58:51 +0100

    Add HTML/Velocity mode switch in decorator editor.

--- src/main/resources/modules/codfish/enhance-decorator-editor.js
+++ src/main/resources/modules/codfish/enhance-decorator-editor.js
@@ -10,9 +10,31 @@ var $ = require("speakeasy/jquery").jQuery,
     codemirror = require("common/codemirror2");
 
 $(function() {
-    console.log("ENHANCE");
-    $('form[name="editdecorator"] textarea[name="content"]').each(function() {
-        console.log("ME", this);
-        codemirror.enhanceTextArea(this, "velocity");
+    var form = $('form[name="editdecorator"]');
+    $('textarea[name="content"]', form).each(function() {
+        codemirror.enhanceTextArea(this, "velocity", function(cm) {
+            cm.getWrapperElement();
+
+            var container = $('<div class="codemirror-mode-switch">Highlight mode: </div>').insertBefore(form);
+
+            function addMode(mode, label) {
+                var radio = $('<input type="radio" name="codemirror-mode"/>').attr('value', mode);
+
+                $('<label/>').text(label).prepend(radio).appendTo(container);
+
+                radio.bind("click", function() {
+                    if (this.checked && cm.getOption("mode") !== this.value) {
+                        cm.setOption("mode", this.value);
+                    }
+                });
+
+                if (cm.getOption("mode") === mode) {
+                    radio.attr("checked", true);
+                }
+            }
+
+            addMode("htmlmixed", "HTML");
+            addMode("velocity", "Velocity");
+        });
     });
 });
